68a9fb5a22765cd6d8c8c296c0faf648e0b79f92
Mitchell Smith <zephyrthenoble@gmail.com>
2015-09-11 10:10:57 -0400

    Simple root node working

--- markov.rs
+++ markov.rs
@@ -1,3 +1,42 @@
+#[derive(Debug)]
+pub struct Root {
+    next: Option<Vec<Node>>,
+    total: u32,
+}
+impl Root{
+    pub fn new() -> Root {
+        Root{next: None, total:0}
+    }
+    pub fn add(&mut self, w: String) {
+        self.total += 1;
+        match self.next {
+            None => {
+                let mut vec: Vec<Node> = Vec::new();
+                let n = Node::from_string(w);
+                vec.push(n);
+                self.next = Some(vec);
+            }
+            Some(ref mut vec) => {
+                let mut flag = false;
+                //search for word
+                for x in vec.iter_mut() {
+                    //if we find the word, add one to part and total
+                    if x.word.value == w {
+                        x.word.part = x.word.part + 1.0;
+                        flag = true;
+                    }
+                }
+                // if the word wasn't in there, create and add it
+                if !flag
+                {
+                    let n = Node::from_string(w);
+                    vec.push(n);
+                }
+            }
+        }
+    }
+}
+#[derive(Debug)]
 pub struct Node {
     pub word: Word,
     next: Option<Vec<Node>>,
@@ -5,45 +44,54 @@ pub struct Node {
 }
 
 impl Node {
-    pub fn new(w: Word) {
-        Node{word:w, next: None, total:0}
+    pub fn new(w: Word) -> Node {
+        Node{word:w, next: None, total:1}
+    }
+    pub fn from_string(s: String) -> Node {
+        Node{word:Word::from_string(s), next: None, total:1}
+    }
+    pub fn is_leaf(&self) -> bool {
+        match self.next {
+            None => return true,
+            Some(ref vec)=> return vec.len() == 0,
+        }
     }
     pub fn add(&mut self, w: String) {
         self.total += 1;
         match self.next {
             None => {
-                let vec = Vec::<Node>new();
-                let n = Node::new(Word::fromString(w));
+                let mut vec: Vec<Node> = Vec::new();
+                let n = Node::from_string(w);
                 vec.push(n);
                 self.next = Some(vec);
             }
-            Some(vec) => {
+            Some(ref mut vec) => {
                 let mut flag = false;
                 //search for word
-                for x in &vec {
+                for x in vec.iter_mut() {
                     //if we find the word, add one to part and total
-                    if(x.word.value == w){
+                    if x.word.value == w {
                         x.word.part = x.word.part + 1.0;
                         flag = true;
                     }
                 }
                 // if the word wasn't in there, create and add it
-                if(!flag)
+                if !flag
                 {
-                    let word = Word::fromString(w);
+                    let n = Node::from_string(w);
+                    vec.push(n);
                 }
             }
         }
     }
 }
-
+#[derive(Debug)]
 pub struct Word {
     pub value: String,
     pub part:  f32
 }
-
 impl Word {
-    pub fn fromString(value: String) -> Word {
+    pub fn from_string(value: String) -> Word {
         Word{ value:value, part:1.0 }
     }
     pub fn new(value: &'static str) -> Word {
@@ -55,5 +103,9 @@ impl Word {
 }
 
 fn main() {
-    let w = Word::new("test");
+    let mut r = Root::new();
+    r.add("test".to_string());
+    r.add("test".to_string());
+    r.add("post".to_string());
+    println!("{:?}", r);
 }
